445d1a1497e8adaeb06838f2504263111b66aea9
Miguel Cardenas <miguel.cardenas@tectijuana.edu.mx>
2020-11-18 16:53:15 -0800

    v5
    Personal of logistic models
    Tech viewmodels

---
+++ v5/ERP/ERP/Global.asax.cs
@@ -0,0 +1,33 @@
+using ERP.Clase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ERP
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            this.CheckRoles();
+            Utilities.CheckSuperUser();
+           
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        private void CheckRoles()
+        {
+            Utilities.CheckRoles("Admin");
+            Utilities.CheckRoles("Technical");
+            Utilities.CheckRoles("Logistic");
+            Utilities.CheckRoles("Supplier");
+            Utilities.CheckRoles("Almacenist");
+        }
+    }
+}
